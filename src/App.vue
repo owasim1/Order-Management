11cdea19a3c5378c356976c6fe3b301d0041820d
owasim1 <omar.wasim@valpo.edu>
2019-09-17 10:49:54 -0700

    second initial

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <OM v-bind:om="om" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OM from './components/OrderManagement.vue'
+window.axios = require('axios')
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    OM
+  },
+
+  mounted: function() {
+    let config = {'user-key': '8ca3c4e4a1dd7de432d90b962ece1fde'};
+    axios.get('https://developers.zomato.com/api/v2.1/search?entity_id=280', {headers: config})
+  },
+
+  data() {
+    return {
+       om: [
+         {id: 1,
+         title: "todo1",
+         completed: false}
+       ]
+    }
   }
 }
 </script>
